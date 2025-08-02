9c284e14d81188e79bdd443b28b6bae8a270b00d
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 16:51:32 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/pulseplus/main.go
@@ -0,0 +1,23 @@
+// cmd/pulseplus/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"pulseplus/internal/pulseplus"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := pulseplus.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
